331388bf12c785661da67a28fd2fac93b41d5534
zeljkosamed <smdkthyli@gmail.com>
2022-06-29 14:46:27 +0300

    IGenericRepository and GenericRepository added.

---
+++ src/Core/InstagramClone.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using InstagramClone.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Application.Interfaces.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        IQueryable<TEntity> GetAll(bool tracking = true);
+        IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> method, bool tracking = true);
+        Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> method, bool tracking = true);
+        Task<TEntity> GetByIdAsync(string id, bool tracking = true);
+        Task<bool> AddAsync(TEntity model);
+        Task<bool> AddRangeAsync(List<TEntity> datas);
+        bool Remove(TEntity model);
+        bool RemoveRange(List<TEntity> datas);
+        Task<bool> RemoveAsync(string id);
+        bool Update(TEntity model);
+        Task<int> SaveAsync();
+    }
+}
---
+++ src/Infrastructure/InstagramClone.Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,99 @@
+﻿using InstagramClone.Application.Interfaces.Repositories;
+using InstagramClone.Domain.Entities;
+using InstagramClone.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Persistence.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly InstagramCloneDbContext _context;
+
+        public GenericRepository(InstagramCloneDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<TEntity> Table => _context.Set<TEntity>();
+
+        public async Task<bool> AddAsync(TEntity model)
+        {
+            EntityEntry<TEntity> entityEntry = await Table.AddAsync(model);
+            return entityEntry.State == EntityState.Added;
+        }
+
+        public async Task<bool> AddRangeAsync(List<TEntity> datas)
+        {
+            await Table.AddRangeAsync(datas);
+            return true;
+        }
+
+        public IQueryable<TEntity> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+        public async Task<TEntity> GetByIdAsync(string id, bool tracking = true)
+        {
+            var query = Table.AsNoTracking();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(i => i.Id == Guid.Parse(id));
+        }
+
+        public async Task<TEntity> GetSingleAsync(Expression<Func<TEntity, bool>> method, bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking)
+                query = query.AsNoTracking();
+            return await query.FirstOrDefaultAsync(method);
+        }
+
+        public IQueryable<TEntity> GetWhere(Expression<Func<TEntity, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            if (!tracking)
+                query = query.AsNoTracking();
+            return query;
+        }
+
+        public bool Remove(TEntity model)
+        {
+            EntityEntry<TEntity> entityEntry = Table.Remove(model);
+            return entityEntry.State == EntityState.Deleted;
+        }
+
+        public async Task<bool> RemoveAsync(string id)
+        {
+            TEntity model = await Table.FirstOrDefaultAsync(data => data.Id == Guid.Parse(id));
+            return Remove(model);
+        }
+
+        public bool RemoveRange(List<TEntity> datas)
+        {
+            Table.RemoveRange(datas);
+            return true;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public bool Update(TEntity model)
+        {
+            EntityEntry<TEntity> entityEntry = Table.Update(model);
+            return entityEntry.State == EntityState.Modified;
+        }
+    }
+}
