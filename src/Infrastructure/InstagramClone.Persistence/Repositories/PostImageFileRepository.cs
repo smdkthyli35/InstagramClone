94f9b15a8be1867df3fb955a7ddb066285c3ae52
zeljkosamed <smdkthyli@gmail.com>
2022-06-29 14:59:48 +0300

    Repositories added.

---
+++ src/Infrastructure/InstagramClone.Persistence/Repositories/PostImageFileRepository.cs
@@ -0,0 +1,18 @@
+﻿using InstagramClone.Application.Interfaces.Repositories;
+using InstagramClone.Domain.Entities;
+using InstagramClone.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Persistence.Repositories
+{
+    public class PostImageFileRepository : GenericRepository<PostImageFile>, IPostImageFileRepository
+    {
+        public PostImageFileRepository(InstagramCloneDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/Infrastructure/InstagramClone.Persistence/Repositories/ProfileRepository.cs
@@ -0,0 +1,18 @@
+﻿using InstagramClone.Application.Interfaces.Repositories;
+using InstagramClone.Domain.Entities;
+using InstagramClone.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstagramClone.Persistence.Repositories
+{
+    public class ProfileRepository : GenericRepository<Profile>, IProfileRepository
+    {
+        public ProfileRepository(InstagramCloneDbContext context) : base(context)
+        {
+        }
+    }
+}
